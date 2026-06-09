4a60570132b3b3022db2f014a4ac1cccadf67167
Cassia Caris <cassia.caris@cerc.com>
2026-06-09 16:11:37 -0300

    alteração do atributo de usuario e senha de id para placeholder

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -8,8 +8,8 @@ test('deve logar com sucesso', async ({page})=>{
 
     await page.goto('http://localhost:3000/login')
 
-    await page.locator('#username').fill(user.username)
-    await page.locator('#password').fill(user.password)
+    await page.locator('input[placeholder="Seu @username incrível"]').fill(user.username)
+    await page.locator('input[placeholder="Digite sua senha secreta"]').fill(user.password)
 
     await page.locator('button[type="submit"]').click()
 
